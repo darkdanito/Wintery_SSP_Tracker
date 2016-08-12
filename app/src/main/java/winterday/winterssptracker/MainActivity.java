0b892e1547c60b4d4c7cd5d0c083dac490bb2679
Biri Biri - YY <darkdanito@hotmail.com>
2016-08-12 20:00:30 +0800

    Version 1.2 - Updated the logic

--- app/src/main/java/winterday/winterssptracker/MainActivity.java
+++ app/src/main/java/winterday/winterssptracker/MainActivity.java
@@ -10,14 +10,32 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    Button button_CH_1_KontaDied;
+    Button button_CH_2_KontaDied;
+    Button button_CH_3_KontaDied;
+
+
     Button button;
     TextView textview;
-    TextView tvCH1Status, tvCH1Timer;
     CountDownTimer countdowntimer;
 
+
+
+
+    TextView tvCH1Status, tvCH1Timer;
+    TextView tvCH2Status, tvCH2Timer;
+    TextView tvCH3Status, tvCH3Timer;
+
+
+
     CountDownTimer countdown1Min;
+    CountDownTimer countDownTimer_CH_1;
+    CountDownTimer countDownTimer_CH_2;
+    CountDownTimer countDownTimer_CH_3;
 
     String channel_1_Status = "Start";
+    String channel_2_Status = "Start";
+    String channel_3_Status = "Start";
 
 
     @Override
@@ -27,10 +45,20 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main_try);
 
         button = (Button)findViewById(R.id.button1);
+        button_CH_1_KontaDied = (Button)findViewById(R.id.bt_CH_1_KontaDied);
+        button_CH_2_KontaDied = (Button)findViewById(R.id.bt_CH_2_KontaDied);
+        button_CH_3_KontaDied = (Button)findViewById(R.id.bt_CH_3_KontaDied);
+
         tvCH1Status = (TextView)findViewById(R.id.tw_CH_1_Status);
         tvCH1Timer = (TextView)findViewById(R.id.tw_CH_1_Time);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        tvCH2Status = (TextView)findViewById(R.id.tw_CH_2_Status);
+        tvCH2Timer = (TextView)findViewById(R.id.tw_CH_2_Time);
+
+        tvCH3Status = (TextView)findViewById(R.id.tw_CH_3_Status);
+        tvCH3Timer = (TextView)findViewById(R.id.tw_CH_3_Time);
+
+/*        button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -39,10 +67,271 @@ public class MainActivity extends Activity {
                 countdowntimer.start();
 
             }
+        });*/
+
+        button_CH_1_KontaDied.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                channel_1_Status = "Before Mining";
+
+                if (countDownTimer_CH_1 != null){
+                    countDownTimer_CH_1.cancel();
+                }
+
+                countDownTimer_CH_1 = new countdown_CH_1(65000, 1000);      // 1 Min
+                countDownTimer_CH_1.start();
+
+            }
+        });
+
+        button_CH_2_KontaDied.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                channel_2_Status = "Before Mining";
+
+                if (countDownTimer_CH_2 != null){
+                    countDownTimer_CH_2.cancel();
+                }
+
+                countDownTimer_CH_2 = new countdown_CH_2(65000, 1000);      // 1 Min
+                countDownTimer_CH_2.start();
+
+            }
+        });
+
+        button_CH_3_KontaDied.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                channel_3_Status = "Before Mining";
+
+                if (countDownTimer_CH_3 != null){
+                    countDownTimer_CH_3.cancel();
+                }
+
+                countDownTimer_CH_3 = new countdown_CH_3(65000, 1000);      // 1 Min
+                countDownTimer_CH_3.start();
+
+            }
         });
     }
 
-    public class CountDownTimerClass extends CountDownTimer {
+    public class countdown_CH_1 extends CountDownTimer {
+
+        public countdown_CH_1(long millisInFuture, long countDownInterval) {
+
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+            int progress = (int) (millisUntilFinished / 1000);
+
+            int seconds = progress % 60;
+            int totalMinutes = progress / 60;
+            int minutes = totalMinutes % 60;
+
+            tvCH1Status.setText(channel_1_Status);
+
+            // TODO: If digit less then 10 need add 0
+            tvCH1Timer.setText(Integer.toString(minutes) + " : " + Integer.toString(seconds));
+
+        }
+
+        @Override
+        public void onFinish() {
+
+            if (channel_1_Status == "Battle Phase"){
+
+                // Before Capture is: 10mins
+                channel_1_Status = "Before Capturing";
+                countDownTimer_CH_1 = new countdown_CH_1(600000, 1000);      // 10 Min
+                countDownTimer_CH_1.start();
+
+            } else if (channel_1_Status == "Before Capturing") {
+
+                // Need Capture is: 12min
+                channel_1_Status = "Capturing";
+                countDownTimer_CH_1 = new countdown_CH_1(120000, 1000);      // 10 Min
+                countDownTimer_CH_1.start();
+
+            } else if (channel_1_Status == "Capturing") {
+
+                // Before Mining is: 1min
+                countdown1Min = new countdown_CH_1(60000, 1000);       // 1 Min
+                channel_1_Status = "Before Mining";
+                countDownTimer_CH_1.start();
+
+            } else if (channel_1_Status == "Before Mining") {
+
+                // Mining is: 32min
+                channel_1_Status = "Mining Phase";
+                countDownTimer_CH_1 = new countdown_CH_1(1920000, 1000);      // 32 Min
+                countDownTimer_CH_1.start();
+
+            } else if (channel_1_Status == "Mining Phase") {
+
+                // Battle Phrase 17mins
+                channel_1_Status = "Battle Phase";
+
+                countDownTimer_CH_1 = new countdown_CH_1(1020000, 1000);      // 1 Min
+                countDownTimer_CH_1.start();
+
+            } else {
+
+                channel_1_Status = "Battle Phase";
+                countDownTimer_CH_1.start();
+            }
+        }
+    }
+
+    public class countdown_CH_2 extends CountDownTimer {
+
+        public countdown_CH_2(long millisInFuture, long countDownInterval) {
+
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+            int progress = (int) (millisUntilFinished / 1000);
+
+            int seconds = progress % 60;
+            int totalMinutes = progress / 60;
+            int minutes = totalMinutes % 60;
+
+            tvCH2Status.setText(channel_1_Status);
+
+            // TODO: If digit less then 10 need add 0
+            tvCH2Timer.setText(Integer.toString(minutes) + " : " + Integer.toString(seconds));
+
+        }
+
+        @Override
+        public void onFinish() {
+
+            if (channel_2_Status == "Battle Phase"){
+
+                // Before Capture is: 10mins
+                channel_2_Status = "Before Capturing";
+                countDownTimer_CH_2 = new countdown_CH_2(600000, 1000);      // 10 Min
+                countDownTimer_CH_2.start();
+
+            } else if (channel_2_Status == "Before Capturing") {
+
+                // Need Capture is: 12min
+                channel_2_Status = "Capturing";
+                countDownTimer_CH_2 = new countdown_CH_2(600000, 1000);      // 10 Min
+                countDownTimer_CH_2.start();
+
+            } else if (channel_2_Status == "Capturing") {
+
+                // Before Mining is: 1min
+                countdown1Min = new countdown_CH_2(60000, 1000);       // 1 Min
+                channel_2_Status = "Before Mining";
+                countDownTimer_CH_2.start();
+
+            } else if (channel_2_Status == "Before Mining") {
+
+                // Mining is: 32min
+                channel_2_Status = "Mining Phase";
+                countDownTimer_CH_2 = new countdown_CH_2(1920000, 1000);      // 32 Min
+                countDownTimer_CH_2.start();
+
+            } else if (channel_2_Status == "Mining Phase") {
+
+                // Battle Phrase 17mins
+                channel_2_Status = "Battle Phase";
+
+                countDownTimer_CH_2 = new countdown_CH_2(1020000, 1000);      // 1 Min
+                countDownTimer_CH_2.start();
+
+            } else {
+
+                channel_2_Status = "Battle Phase";
+                countDownTimer_CH_2.start();
+            }
+        }
+    }
+
+    public class countdown_CH_3 extends CountDownTimer {
+
+        public countdown_CH_3(long millisInFuture, long countDownInterval) {
+
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+            int progress = (int) (millisUntilFinished / 1000);
+
+            int seconds = progress % 60;
+            int totalMinutes = progress / 60;
+            int minutes = totalMinutes % 60;
+
+            tvCH3Status.setText(channel_1_Status);
+
+            // TODO: If digit less then 10 need add 0
+            tvCH3Timer.setText(Integer.toString(minutes) + " : " + Integer.toString(seconds));
+
+        }
+
+        @Override
+        public void onFinish() {
+
+            if (channel_3_Status == "Battle Phase"){
+
+                // Before Capture is: 10mins
+                channel_3_Status = "Before Capturing";
+                countDownTimer_CH_3 = new countdown_CH_3(600000, 1000);      // 10 Min
+                countDownTimer_CH_3.start();
+
+            } else if (channel_3_Status == "Before Capturing") {
+
+                // Need Capture is: 12min
+                channel_3_Status = "Capturing";
+                countDownTimer_CH_3 = new countdown_CH_3(600000, 1000);      // 10 Min
+                countDownTimer_CH_3.start();
+
+            } else if (channel_3_Status == "Capturing") {
+
+                // Before Mining is: 1min
+                countdown1Min = new countdown_CH_3(60000, 1000);       // 1 Min
+                channel_3_Status = "Before Mining";
+                countDownTimer_CH_3.start();
+
+            } else if (channel_3_Status == "Before Mining") {
+
+                // Mining is: 32min
+                channel_3_Status = "Mining Phase";
+                countDownTimer_CH_3 = new countdown_CH_3(1920000, 1000);      // 32 Min
+                countDownTimer_CH_3.start();
+
+            } else if (channel_1_Status == "Mining Phase") {
+
+                // Battle Phrase 17mins
+                channel_3_Status = "Battle Phase";
+
+                countDownTimer_CH_3 = new countdown_CH_3(1020000, 1000);      // 1 Min
+                countDownTimer_CH_3.start();
+
+            } else {
+
+                channel_3_Status = "Battle Phase";
+                countDownTimer_CH_3.start();
+            }
+        }
+    }
+
+/*    public class CountDownTimerClass extends CountDownTimer {
 
         public CountDownTimerClass(long millisInFuture, long countDownInterval) {
 
@@ -101,5 +390,5 @@ public class MainActivity extends Activity {
                 countdowntimer.start();
             }
         }
-    }
+    }*/
 }
