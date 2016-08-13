e1c7dae6616dac49f5db183c2f61befd81d37303
Biri Biri - YY <darkdanito@hotmail.com>
2016-08-13 19:29:27 +0800

    Version 1.5.2 - Code Cleanup

--- app/src/main/java/winterday/winterssptracker/MainActivity.java
+++ app/src/main/java/winterday/winterssptracker/MainActivity.java
@@ -46,7 +46,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_try);
 
-        button = (Button)findViewById(R.id.button1);
         button_CH_1_KontaDied = (Button)findViewById(R.id.bt_CH_1_KontaDied);
         button_CH_2_KontaDied = (Button)findViewById(R.id.bt_CH_2_KontaDied);
         button_CH_3_KontaDied = (Button)findViewById(R.id.bt_CH_3_KontaDied);
@@ -59,17 +58,6 @@ public class MainActivity extends Activity {
 
         tvCH3Status = (TextView)findViewById(R.id.tw_CH_3_Status);
         tvCH3Timer = (TextView)findViewById(R.id.tw_CH_3_Time);
-
-/*        button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                countdowntimer = new CountDownTimerClass(3000, 1000);
-                countdowntimer.start();
-
-            }
-        });*/
 
         button_CH_1_KontaDied.setOnClickListener(new View.OnClickListener() {
 
@@ -347,65 +335,4 @@ public class MainActivity extends Activity {
             }
         }
     }
-
-/*    public class CountDownTimerClass extends CountDownTimer {
-
-        public CountDownTimerClass(long millisInFuture, long countDownInterval) {
-
-            super(millisInFuture, countDownInterval);
-
-        }
-
-        @Override
-        public void onTick(long millisUntilFinished) {
-
-            int progress = (int) (millisUntilFinished/1000);
-
-            tvCH1Status.setText(channel_1_Status);
-            tvCH1Timer.setText(Integer.toString(progress));
-
-        }
-
-        @Override
-        public void onFinish() {
-
-            if (channel_1_Status == "Battle Phase"){
-
-                // Before Capture is 10mins
-                channel_1_Status = "Before Capturing";
-                countdowntimer.start();
-
-            } else if (channel_1_Status == "Before Capturing") {
-
-                // Need Capture timing
-                channel_1_Status = "Capturing";
-                countdowntimer.start();
-
-            } else if (channel_1_Status == "Capturing") {
-
-                // Before Mining is: 1min
-                countdown1Min = new CountDownTimerClass(5000, 1000);
-                channel_1_Status = "Before Mining";
-                countdown1Min.start();
-
-            } else if (channel_1_Status == "Before Mining") {
-
-                // Mining is: 32min
-                channel_1_Status = "Mining Phase";
-                countdowntimer.start();
-
-            } else if (channel_1_Status == "Mining Phase") {
-
-                // Battle Phrase 17mins
-                channel_1_Status = "Battle Phase";
-                countdowntimer.start();
-
-
-            } else {
-
-                channel_1_Status = "Battle Phase";
-                countdowntimer.start();
-            }
-        }
-    }*/
 }
