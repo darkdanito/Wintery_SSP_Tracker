77aeb00bedd121cfcec5585d8554f04dffb1f964
Biri Biri - YY <darkdanito@hotmail.com>
2016-08-13 22:29:14 +0800

    Version 1.7 - Added Semi Logic for manual update for CH 1 timer

--- app/src/main/java/winterday/winterssptracker/MainActivity.java
+++ app/src/main/java/winterday/winterssptracker/MainActivity.java
@@ -4,17 +4,22 @@ import android.app.Activity;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener {
 
     Button button_CH_1_KontaDied;
     Button button_CH_2_KontaDied;
     Button button_CH_3_KontaDied;
+
+    Button button_CH_1_Update;
 
 
     Button button;
@@ -23,11 +28,15 @@ public class MainActivity extends Activity {
 
 
 
+    String spinnerCH1_selected;
+
+
 
     TextView tvCH1Status, tvCH1Timer;
     TextView tvCH2Status, tvCH2Timer;
     TextView tvCH3Status, tvCH3Timer;
 
+    EditText edCH1Update;
 
 
     CountDownTimer countdown1Min;
@@ -52,6 +61,10 @@ public class MainActivity extends Activity {
         button_CH_2_KontaDied = (Button)findViewById(R.id.bt_CH_2_KontaDied);
         button_CH_3_KontaDied = (Button)findViewById(R.id.bt_CH_3_KontaDied);
 
+        button_CH_1_Update = (Button)findViewById(R.id.bt_CH_1_update);
+
+        edCH1Update = (EditText)findViewById(R.id.ed_CH_1);
+
         tvCH1Status = (TextView)findViewById(R.id.tw_CH_1_Status);
         tvCH1Timer = (TextView)findViewById(R.id.tw_CH_1_Time);
 
@@ -61,11 +74,11 @@ public class MainActivity extends Activity {
         tvCH3Status = (TextView)findViewById(R.id.tw_CH_3_Status);
         tvCH3Timer = (TextView)findViewById(R.id.tw_CH_3_Time);
 
-        Spinner spin_CH_1 = (Spinner) findViewById(R.id.spinner_CH_1);
+        final Spinner spin_CH_1 = (Spinner) findViewById(R.id.spinner_CH_1);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.array_SSP_Phrase, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spin_CH_1.setAdapter(adapter);
-
+        spin_CH_1.setOnItemSelectedListener(this);
 
         button_CH_1_KontaDied.setOnClickListener(new View.OnClickListener() {
 
@@ -80,7 +93,6 @@ public class MainActivity extends Activity {
 
                 countDownTimer_CH_1 = new countdown_CH_1(60000, 1000);      // 1 Min
                 countDownTimer_CH_1.start();
-
             }
         });
 
@@ -97,7 +109,6 @@ public class MainActivity extends Activity {
 
                 countDownTimer_CH_2 = new countdown_CH_2(60000, 1000);      // 1 Min
                 countDownTimer_CH_2.start();
-
             }
         });
 
@@ -114,10 +125,55 @@ public class MainActivity extends Activity {
 
                 countDownTimer_CH_3 = new countdown_CH_3(60000, 1000);      // 1 Min
                 countDownTimer_CH_3.start();
+            }
+        });
 
+        button_CH_1_Update.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String getMins = "";
+
+                Log.d("Necro","Necro: " + edCH1Update.getText().toString() + " : " + spinnerCH1_selected);
+
+                String userInputCH1Timing = edCH1Update.getText().toString();
+
+                String getSeconds = userInputCH1Timing.substring(Math.max(userInputCH1Timing.length() - 2, 0));
+
+                if (userInputCH1Timing.length() == 4){
+
+                    getMins = userInputCH1Timing.substring(0,2);
+
+                } else if (userInputCH1Timing.length() == 3) {
+
+                    getMins = userInputCH1Timing.substring(0,1);
+
+                }
+
+                channel_1_Status = spinnerCH1_selected;
+
+                if (countDownTimer_CH_1 != null){
+                    countDownTimer_CH_1.cancel();
+                }
+
+                countDownTimer_CH_1 = new countdown_CH_1(60000, 1000);      // 1 Min
+                countDownTimer_CH_1.start();
             }
         });
     }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        spinnerCH1_selected = parent.getItemAtPosition(pos).toString();
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
 
     public class countdown_CH_1 extends CountDownTimer {
 
